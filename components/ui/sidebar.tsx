10eeaec623a136558222e796038f546707e55f77
iammatthewhenry <matthew@iammatthewhenry.com>
2026-03-13 12:17:24 -0700

    Update sidebar.tsx

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -3,7 +3,6 @@ import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { VariantProps, cva } from "class-variance-authority";
 import { PanelLeftIcon } from "lucide-react";
-
 import { useIsMobile } from "@/components/ui/use-mobile";
 import { cn } from "@/components/ui/utils";
 import { Button } from "./button";
